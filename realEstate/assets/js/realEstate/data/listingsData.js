f32451c828ec3f7699dfe688c0f012463d81b463
Ana Nikolova <ani.nik19@gmail.com>
2019-06-27 07:03:09 +0200

    dummy data done

---
+++ realEstate/assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,74 @@
+var  ListingsData = [
+  {
+    address: "Karal-Marx-Alee 76",
+    city:"Berlin",
+    state:"Berlin",
+    rooms: 3,
+    price: 19600000,
+    floorSpace: 2000,
+    extras:[
+      "elevator",
+      "gym"
+    ],
+    homeType: "apartament",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjZhyp1XfkqsBB-5WEx4IQtyeHYHukua5TK6zosWuGcXlHr9rlVA"
+  },
+  {
+    address: "Charlotten Strasse 3",
+    city:"Düsseldorf",
+    state:"WF",
+    rooms: 4,
+    price: 15400000,
+    floorSpace: 1000,
+    extras:[
+      "elevator",
+      "gym"
+    ],
+    homeType: "apartament",
+    image:"https://www.engelvoelkers.com/images/5c221d7b-6163-442c-a4ca-7cb5bf13382b/beautiful-apartment-in-ciutat-vella?w=576&h=432"
+  },
+  {
+    address: "Rupprecher Strasse 21",
+    city:"Münich",
+    state:"M",
+    rooms: 2,
+    price: 12300000,
+    floorSpace: 1500,
+    extras:[
+      "elevator",
+      "gym"
+    ],
+    homeType: "apartament",
+    image:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinwqnx7YjjAhWLY1AKHeo0BkUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fde%2Fluxurioses-penthouse-m-blick-auf-museuminsel-whirlpool-dachterrasse-sauna.de.html&psig=AOvVaw16Aye-3Ozd-QPZWGFgNfCt&ust=1561697122317800"
+  },
+  {
+    address: "Münsterstrasse 123",
+    city:"Frankfurt",
+    state:"FN",
+    rooms: 3,
+    price: 87200000,
+    floorSpace: 3400,
+    extras:[
+      "elevator",
+      "gym"
+    ],
+    homeType: "apartament",
+    image:"https://www.callsen.de/images/ferienwohnungen/binz/villamathilde/ferienwohnung_22/10_Wohnzimmer.jpg"
+  },
+  {
+    address: "FrankfurterStrasse 5",
+    city:"Postam",
+    state:"Berlin",
+    rooms: 4,
+    price: 54300000,
+    floorSpace: 4300,
+    extras:[
+      "elevator",
+      "gym"
+    ],
+    homeType: "apartament",
+    image:"https://www.idesignarch.com/wp-content/uploads/Berlin-Mitte-Art-Loft_5.jpg"
+  }
+]
+
+export default  ListingsData;
--- realEstate/public/js/components/realEstate.js
+++ realEstate/public/js/components/realEstate.js
@@ -757,7 +757,72 @@ exports.default = Header;
 
 /***/ }),
 
-/***/ 236:
+/***/ 233:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var ListingsData = [{
+  address: "Karal-Marx-Alee 76",
+  city: "Berlin",
+  state: "Berlin",
+  rooms: 3,
+  price: 19600000,
+  floorSpace: 2000,
+  extras: ["elevator", "gym"],
+  homeType: "apartament",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjZhyp1XfkqsBB-5WEx4IQtyeHYHukua5TK6zosWuGcXlHr9rlVA"
+}, {
+  address: "Charlotten Strasse 3",
+  city: "Düsseldorf",
+  state: "WF",
+  rooms: 4,
+  price: 15400000,
+  floorSpace: 1000,
+  extras: ["elevator", "gym"],
+  homeType: "apartament",
+  image: "https://www.engelvoelkers.com/images/5c221d7b-6163-442c-a4ca-7cb5bf13382b/beautiful-apartment-in-ciutat-vella?w=576&h=432"
+}, {
+  address: "Rupprecher Strasse 21",
+  city: "Münich",
+  state: "M",
+  rooms: 2,
+  price: 12300000,
+  floorSpace: 1500,
+  extras: ["elevator", "gym"],
+  homeType: "apartament",
+  image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinwqnx7YjjAhWLY1AKHeo0BkUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fde%2Fluxurioses-penthouse-m-blick-auf-museuminsel-whirlpool-dachterrasse-sauna.de.html&psig=AOvVaw16Aye-3Ozd-QPZWGFgNfCt&ust=1561697122317800"
+}, {
+  address: "Münsterstrasse 123",
+  city: "Frankfurt",
+  state: "FN",
+  rooms: 3,
+  price: 87200000,
+  floorSpace: 3400,
+  extras: ["elevator", "gym"],
+  homeType: "apartament",
+  image: "https://www.callsen.de/images/ferienwohnungen/binz/villamathilde/ferienwohnung_22/10_Wohnzimmer.jpg"
+}, {
+  address: "FrankfurterStrasse 5",
+  city: "Postam",
+  state: "Berlin",
+  rooms: 4,
+  price: 54300000,
+  floorSpace: 4300,
+  extras: ["elevator", "gym"],
+  homeType: "apartament",
+  image: "https://www.idesignarch.com/wp-content/uploads/Berlin-Mitte-Art-Loft_5.jpg"
+}];
+
+exports.default = ListingsData;
+
+/***/ }),
+
+/***/ 237:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -785,6 +850,10 @@ var _Listings = __webpack_require__(232);
 
 var _Listings2 = _interopRequireDefault(_Listings);
 
+var _listingsData = __webpack_require__(233);
+
+var _listingsData2 = _interopRequireDefault(_listingsData);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -802,7 +871,7 @@ var App = function (_Component) {
     var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
 
     _this.state = {
-      name: 'Joe'
+      listingsData: _listingsData2.default
     };
     return _this;
   }
@@ -810,6 +879,7 @@ var App = function (_Component) {
   _createClass(App, [{
     key: 'render',
     value: function render() {
+      console.log(this.state.listingsData);
       return _react2.default.createElement(
         'div',
         null,
@@ -833,4 +903,4 @@ _reactDom2.default.render(_react2.default.createElement(App, null), app);
 
 /***/ })
 
-},[236]);
+},[237]);
